94031d9bed82f8a0a1da805a5b71d37b4e8ac34b
jth0mass0n <jonnerin@gmail.com>
2018-10-22 07:50:49 -0700

    refactored MainRecycleView to load data when it first inits.

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from os.path import dirname, join
 from kivy.lang import Builder
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.properties import NumericProperty, StringProperty, BooleanProperty,\
     ListProperty
 from kivy.clock import Clock
 from kivy.animation import Animation
 from kivy.uix.screenmanager import Screen
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 import sqlite3
+
 
 con = sqlite3.connect('smartiepi.db')
 cur = con.cursor()
@@ -41,13 +47,7 @@ class SmartiePiApp(App):
         self.available_screens = [join(curdir, 'screens',
             '{}.kv'.format(fn).lower()) for fn in self.available_screens]
         sm.switch_to(self.load_screen(0), direction='left') #load main screen
-        self.main_load_tab_data(0)
         return hub
-
-    def main_load_tab_data(self, idx):
-        cur.execute("SELECT NodeId, MessageId, TimeStamp FROM NodeMessages")
-        self.rows = cur.fetchall()
-        print(self.rows)
 
     def go_screen(self, idx):
         self.index = idx
@@ -62,6 +62,16 @@ class SmartiePiApp(App):
 
     def update_clock(self, dt):
         self.root.ids.date_and_time.text = time.strftime("%I:%M %p\n%m/%d/%Y")
+
+class MainRecycleView(RecycleView):
+    def __init__(self, **kwargs):
+        super(MainRecycleView, self).__init__(**kwargs)
+
+        cur.execute("SELECT NodeId, MessageId, TimeStamp FROM NodeMessages ORDER by Id DESC")
+        #self.rows = cur.fetchall()
+
+        self.data = [{'text':"NodeId:{} MessageId:{} TimeStamp:{}".format(NodeId,MessageId,TimeStamp)} for NodeId,MessageId,TimeStamp in cur.fetchall()]
+        #print(self.data)
         
 
 if __name__ == '__main__':
